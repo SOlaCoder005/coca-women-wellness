292b212808d64fe199c002f3d30533e0342c39ed
SOlaCoder005 <80408484+SOlaCoder005@users.noreply.github.com>
2021-04-30 21:55:15 +0100

    added template footer and menu with a little bit of java script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,22 @@
 //Nav Menu feature
 const menuIcon = document.querySelector('#menuIcon');
 const nav = document.querySelector('nav');
+const jointIcon = document.querySelectorAll('.jointicon');
+const hamburger = document.querySelector('#hamburger');
 
 menuIcon.addEventListener("click", () => {
-    nav.classList.toggle('open') 
-}
+    nav.classList.toggle('open');
+    jointicon.forEach(icon => {
+        icon.classList.toggle('hidden');
+    });
+});
 
-)
+window.addEventListener("resize", () => {
+    if(document.body.clientWidth > 720) {
+        nav.classList.remove('open');
+        jointicon.forEach(icon=> {
+            icon.classList.add('hidden')
+        });
+        hamburger.classList.remove('hidden')
+    }
+});
